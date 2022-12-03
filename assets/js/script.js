0ecafdcef32c56fdd292a133cf4f6a3da2fc00a9
HannaBerggren <hannajberggren@gmail.com>
2022-12-03 12:20:15 +0000

    Add function to play the game

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+/**
+ * Create game variables
+ */
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
+}
+
+/**
+ * Create const variables to play the game
+ */
+const playGame = () => {
+    const rockButton = document.querySelector('.rock');
+    const paperButton = document.querySelector('.paper');
+    const scissorButton = document.querySelector('.scissors');
+    const playerOptions = [rockButton, paperButton, scissorButton];
+    const computerOptions = ['rock', 'paper', 'scissors'];
+  
+}  
+
+/** 
+ * Function to play the game
+ */  
+const playGame = () => {
+    const rockButton = document.querySelector('.rock');
+    const paperButton = document.querySelector('.paper');
+    const scissorButton = document.querySelector('.scissors');
+    const playerOptions = [rockButton, paperButton, scissorButton];
+    const computerOptions = ['rock', 'paper', 'scissors'];
+}
+
