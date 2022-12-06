24e889c7eb3adb0c169415c443a0dcc47cadd3b3
HannaBerggren <hannajberggren@gmail.com>
2022-12-06 07:14:44 +0000

    Add changes to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
 /**
- * create game variables
- */
-const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
-    let moves = 0;
-}
-
-/**
  * create const variables to play the game
  */
-const playGame = () => {
-    const rockButton = document.querySelector('.rock');
-    const paperButton = document.querySelector('.paper');
-    const scissorButton = document.querySelector('.scissors');
-    const playerOptions = [rockButton, paperButton, scissorButton];
-    const computerOptions = ['rock', 'paper', 'scissors'];
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+  let moves = 0;
 
-/** 
- * Function to start play
+/**
+ * Function to play game
  */ 
-playerOptions.forEach(option => {
+const playGame = () => {
+  const rockButton = document.querySelector('.rock');
+  const paperButton = document.querySelector('.paper');
+  const scissorButton = document.querySelector('.scissors');
+  const playerOptions = [rockButton, paperButton, scissorButton];
+  const computerOptions = ['rock', 'paper', 'scissors'];
+
+
+/**
+ * Function to start play
+ */
+  playerOptions.forEach(option => {
     option.addEventListener('click', function() {
 
     const movesLeft = document.querySelector('.movesLeft'); moves++;
@@ -29,75 +29,76 @@ playerOptions.forEach(option => {
       const choiceNumber = Math.floor(Math.random()*3);
       const computerChoice = computerOptions[choiceNumber];
 
-/** 
+/**
  * Function to see who wins
  */
-winner(this.innerText, computerChoice);
+        winner(this.innerText, computerChoice);
 
-/** 
- *  Function to end game after 10 moves
-*/
-if(moves === 10) {
-    gameOver(playerOptions, movesLeft);
-  }
-});
-});
+/**
+ * Function to end game after 10moves
+ */
+        if(moves === 10) {
+          gameOver(playerOptions, movesLeft);
+        }
+    });
+  });
 };
 
-/** 
+/**
  * Function to decide the winner
- */ 
+ */
 const winner = (player, computer) => {
-    const result = document.querySelector('.result');
-    const playerScoreBoard = document.querySelector('.p-count');
-    const computerScoreBoard = document.querySelector('.c-count');
-      player = player.toLowerCase();
-      computer = computer.toLowerCase();
-      
-      if (player === computer) {
-        result.textContent = 'Tie';
-        } else if (player == 'rock') {
-          if (computer == 'paper'){
-            result.textContent = 'Computer Won';
+  const result = document.querySelector('.result');
+  const playerScoreBoard = document.querySelector('.player-count');
+  const computerScoreBoard = document.querySelector('.computer-count');
+    player = player.toLowerCase();
+    computer = computer.toLowerCase();
+    
+    if (player === computer) {
+      result.textContent = 'Tie';
+      } else if (player == 'rock') {
+        if (computer == 'paper'){
+          result.textContent = 'Computer Won';
+          computerScore++;
+          computerScoreBoard.textContent = computerScore;
+        } else {
+            result.textContent = 'Player Win!';
+            playerScore++;
+            playerScoreBoard.textContent = playerScore;
+        }
+    } else if (player == 'scissors') {
+        if (computer == 'rock'){
+            result.textContent = 'Computer Win!';
             computerScore++;
             computerScoreBoard.textContent = computerScore;
-          } else {
-              result.textContent = 'Player Win!';
-              playerScore++;
-              playerScoreBoard.textContent = playerScore;
-          }
-      } else if (player == 'scissors') {
-          if (computer == 'rock'){
-              result.textContent = 'Computer Win!';
-              computerScore++;
-              computerScoreBoard.textContent = computerScore;
-          } else {
-              result.textContent = 'Player Win!';
-              playerScore++;
-              playerScoreBoard.textContent = playerScore;
-          }
-      } else if (player == 'paper'){
-          if (computer == 'scissors'){
-              result.textContent = 'Computer Win!';
-              computerScore++;
-              computerScoreBoard.textContent = computerScore;
-          } else {
-              result.textContent = 'Player Win!';
-              playerScore++;
-              playerScoreBoard.textContent = playerScore;
-          }
-      }
-  };
-  /**
-   * Function to run when game is over
-   */ 
+        } else {
+            result.textContent = 'Player Win!';
+            playerScore++;
+            playerScoreBoard.textContent = playerScore;
+        }
+    } else if (player == 'paper'){
+        if (computer == 'scissors'){
+            result.textContent = 'Computer Win!';
+            computerScore++;
+            computerScoreBoard.textContent = computerScore;
+        } else {
+            result.textContent = 'Player Win!';
+            playerScore++;
+            playerScoreBoard.textContent = playerScore;
+        }
+    }
+};
+
+/**
+ * Function to run when game is over
+ */
   const gameOver = (playerOptions, movesLeft) => {
 
-    const chooseMove = document.querySelector('.move');
+    const chooseMove = document.querySelector('.choose');
     const result = document.querySelector('.result');
-    const reloadButton = document.querySelector('.reload');
-
-    playerOptions.forEach(option => {
+    const reloadButton = document.querySelector('.restart');
+     
+      playerOptions.forEach(option => {
         option.style.display = 'none';
       });
 
@@ -123,11 +124,21 @@ const winner = (player, computer) => {
           window.location.reload();
       });
   };
-  /**
-   * Calling playGame function
-   */ 
+
+/**
+ * Calling playGame function
+ */
   playGame();
 
-  // Calling game function
-  game();
+};
+
+/**
+ * Calling game function
+ */
+game();
+  
+
+
+
+
 
